2827b5c9369aefb7983cbe2dc87c06b189fe0a22
Youssef <youssef1995elam@gmail.com>
2023-12-29 14:37:00 +0100

    Add cn className

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import Image from 'next/image'
+import searchSvg from '../../public/search.svg'
+import { cn } from '@/utils/cn'
 
-export default function SearchBox() {
+type Props = {
+    className?:string,
+    value: string,
+    onChange: React.ChangeEventHandler<HTMLInputElement> | undefined,
+    onSubmit: React.FormEventHandler<HTMLFormElement> | undefined,
+}
+
+export default function SearchBox(props: Props) {
   return (
-    <form className="flex relative items-center justify-center h-10">
-
+    <form onSubmit={props.onSubmit} className={cn(
+        "flex relative items-center justify-center h-10",
+        props.className
+    )}>
+        <input 
+            value={props.value}
+            onChange={props.onChange}
+            type="text" 
+            placeholder="Search location..." 
+            className="px-4 py-2 w-[230px] border border-gray-300 rounded-l-md focus:outline-none focus:border-yellow-500 h-full" />
+        <button className="px-4 py-[9px] bg-yellow-400 text-white rounded-r-md focus:outline-none hover:bg-yellow-500 h-full">
+            <Image
+                src={searchSvg}
+                alt="Search SVG"
+                width={23}
+                height={23}
+            />
+        </button>
     </form>
   )
 }
