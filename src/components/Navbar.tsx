ab22fca501fdaf4df9687bc4ec869a2fe73ff157
Youssef <youssef1995elam@gmail.com>
2024-01-01 14:57:27 +0100

    Fix navbar responsibity

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -68,36 +68,64 @@ export default function Navbar({
         }
     }
 
+    function handleCurrentLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(async (position) => {
+                const { latitude, longitude } = position.coords
+                try {
+                    setLoadingPlace(true)
+                    const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`)
+                    setTimeout(() => {
+                        setLoadingPlace(false)
+                        setPlace(response.data.name)
+                    }, 500)
+                } catch (error) {
+                    setLoadingPlace(false)
+                }
+            })
+        }
+    }
+
     return (
-        <nav className="shadow-sm sticky top-0 left-0 z-50 bg-white">
-            <div className="h-[80px] w-full flex justify-between items-center max-w-7xl px-3 mx-auto">
-                <div className="flex items-center justify-center gap-2">
-                    <h2 className="text-gray-500 text-3xl">Weather App</h2>
-                    <Image 
-                        src={weatherSvg}
-                        alt="Weather App SVG"
-                        width={50}
-                        height={50}
-                        className="mt-1"
-                    />
-                </div>
-                {/*      */}
-                <section className="flex gap-2 items-center">
-                    <Image 
-                        src={currentLocationSvg}
-                        alt="Location SVG"
-                        width={40}
-                        height={40}
-                        className="hover:opacity-80 cursor-pointer"
-                    />
-                    <p className="text-slate-900/80 text-sm">{location}</p>
-                    <div className="relative">
-                        <SearchBox value={city} onChange={(e) => handleInputChange(e.target.value)} onSubmit={handleSubmitSearch} />
-                        <SuggestionBox {...{showSuggestions, suggestions, handleSuggestionClick, error}} />
+        <>
+            <nav className="shadow-sm sticky top-0 left-0 z-50 bg-white">
+                <div className="h-[80px] w-full flex justify-between items-center max-w-7xl px-3 mx-auto">
+                    <div className="flex items-center justify-center gap-2">
+                        <h2 className="text-gray-500 text-3xl">Weather App</h2>
+                        <Image 
+                            src={weatherSvg}
+                            alt="Weather App SVG"
+                            width={50}
+                            height={50}
+                            className="mt-1"
+                        />
                     </div>
-                </section>
-            </div>
-        </nav>
+                    {/*      */}
+                    <section className="flex gap-2 items-center">
+                        <Image 
+                            title="Your current location"
+                            onClick={handleCurrentLocation}
+                            src={currentLocationSvg}
+                            alt="Location SVG"
+                            width={40}
+                            height={40}
+                            className="hover:opacity-80 cursor-pointer"
+                        />
+                        <p className="text-slate-900/80 text-sm">{location}</p>
+                        <div className="relative hidden md:flex">
+                            <SearchBox value={city} onChange={(e) => handleInputChange(e.target.value)} onSubmit={handleSubmitSearch} />
+                            <SuggestionBox {...{showSuggestions, suggestions, handleSuggestionClick, error}} />
+                        </div>
+                    </section>
+                </div>
+            </nav>
+            <section className="flex max-w-7xl px-3 md:hidden">
+                <div className="relative">
+                                <SearchBox value={city} onChange={(e) => handleInputChange(e.target.value)} onSubmit={handleSubmitSearch} />
+                                <SuggestionBox {...{showSuggestions, suggestions, handleSuggestionClick, error}} />
+                </div>
+            </section>
+        </>
     )
 }
 
